0cc5f0864bf60718add055c01016e1da053c4c15
vinicoloco <vinicius.inacio1997@gmail.com>
2022-03-16 15:10:30 -0300

    novas aulas

---
+++ aula02/aula02.py
@@ -0,0 +1,5 @@
+canal = "CFB Cursos"
+curso = "Curso de Python"
+print(canal + " - " + curso)
+
+#comentarios são feitos com # ou """ """ quando for mais de uma linha.
---
+++ aula03/aula03.py
@@ -0,0 +1,9 @@
+num1=num2=res=0
+
+def cn():
+    global canal
+    canal="CFB Cursos"
+
+cn()
+
+print(canal)
